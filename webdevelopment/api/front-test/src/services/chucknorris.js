adebf2bf152fa94ccee9262da634a4dd80378292
sidi1602 <111370786+sidi1602@users.noreply.github.com>
2023-01-27 14:52:08 +0100

    Delete webdevelopment directory

--- webdevelopment/api/front-test/src/services/chucknorris.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const getRandomFact = async () => {
-    const response = await axios.get('https://api.chucknorris.io/jokes/random');
-    return response.data
-}
--- webdevelopment/api/server/middlewares/auth.ts
+++
@@ -1,9 +0,0 @@
-import {Request, Response} from 'express';
-
-export const isAuth = (request: any, response: Response, next: any) => {
-    if (request.session.user) {
-        next()
-    } else {
-        return response.status(503).json({msg: "Not authenticated !"})
-    }
-}
